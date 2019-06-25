f3dcf248a321acedfef16517399111f69d43ca9d
Kevin <19938583+kev-vin@users.noreply.github.com>
2019-06-25 09:07:40 -0500

    Implement database

--- gris.js
+++ gris.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const sqlite = require('sqlite3').verbose();
+let database;
+
 class Grid {
     constructor (_width, _height) {
         this.width  = _width;
@@ -12,6 +15,30 @@ class Grid {
                 row.push("white"); //rgb(255, 255, 255); white
             }
             this.grid.push(row);
-    }
+        }
+        database = new sqlite.Database('./bot.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE, (err) => {
+            if(err) {
+                throw err;
+            }
+            console.log("Connected to the database!"); 
+        });
+        //create tables if they don't already exist
+        database.serialize(() => {
+            database.run(`CREATE TABLE IF NOT EXISTS grid (
+                x INT,
+                y INT,
+                color INT,
+                PRIMARY KEY (x,y)
+            );`);
+
+            database.run(`CREATE TABLE IF NOT EXISTS brushes (
+                guild VARCHAR(64),
+                x INT,
+                y INT,
+                PRIMARY KEY (guild)
+            );`);
+
+            //TODO: load existing brushes and existing tiles into memory
+        });
 }
 module.exports = Grid;
